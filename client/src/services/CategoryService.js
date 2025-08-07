d60ad9c23437d58c3bf297d46ffcf304716e9ba5
duynhan1407 <nhancao1103@gmail.com>
2025-08-07 09:45:31 +0700

    Categoty

--- client/src/services/CategoryService.js
+++ client/src/services/CategoryService.js
@@ -1,6 +1,7 @@
+
 import axios from 'axios';
 
-const API_URL = `${process.env.REACT_APP_API_URL}/categories`;
+const API_URL = `${process.env.REACT_APP_API_URL}/api/categories`;
 
 export const getAllCategories = async () => {
   const res = await axios.get(API_URL);
